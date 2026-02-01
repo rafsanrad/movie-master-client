dcd1a6d72fde884a9ec345626bbb6836215e0028
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-02-01 21:29:34 +0600

    added navbar

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { Outlet } from "react-router";
+import { Toaster } from "react-hot-toast";
+import Footer from "../components/Footer";
+import NavBar from "../components/Navbar";
 
 const MainLayout = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto">
+        <NavBar />
+        <div className="mt-4">
+          <Outlet />
         </div>
-    );
+        <Footer/>
+      </div>
+
+      <Toaster/>
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from './router/routes.jsx'
+import { RouterProvider } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
