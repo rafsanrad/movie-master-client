6cd360973174a4ad17e2d034e2780b9d9fcf08a7
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-02-02 00:36:06 +0600

    added authProvider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from './AuthContext';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+import { auth } from '../firebase/firebase.config';
 
-const AuthProvider = () => {
+
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const signInWithGoogle = () =>{
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const updateUserProfile = (displayName, photoURL) => {
+            return updateProfile(auth.currentUser, {displayName, photoURL})
+    }
+
+    const signOutUser = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            // console.log(currentUser)
+            setLoading(false);
+        })
+
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    const authInfo = {
+        createUser,
+        updateUserProfile,
+        signInUser,
+        signInWithGoogle,
+        signOutUser,
+        user,
+        loading
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
     );
 };
 
