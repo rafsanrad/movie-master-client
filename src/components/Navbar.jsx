c75bb73372b47bca42bf9eb5659ad402d8b4456c
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-02-02 01:11:23 +0600

    added user drop down

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,22 @@
-import React from "react";
+import React, { use } from "react";
 import { Link, NavLink } from "react-router";
 import MyLink from "./MyLink";
+import { AuthContext } from "../context/AuthContext";
+import { FaUser } from "react-icons/fa";
+import { IoLogIn, IoLogOut } from "react-icons/io5";
 
 const Navbar = () => {
+  const { user, signOutUser } = use(AuthContext);
   const links = (
     <>
       <li>
-        <MyLink
-          to="/"
-        >
-          Home
-        </MyLink>
+        <MyLink to="/">Home</MyLink>
       </li>
       <li>
-        <MyLink
-          to="/all-movies"
-        >
-          All Movies
-        </MyLink>
+        <MyLink to="/all-movies">All Movies</MyLink>
       </li>
       <li>
-        <MyLink
-          to="/profile"
-        >
-          Profile
-        </MyLink>
-      </li>
-      <li>
-        <MyLink
-          to="/add-movie"
-        >
-          Add Movie
-        </MyLink>
+        <MyLink to="/add-movie">Add Movie</MyLink>
       </li>
     </>
   );
@@ -41,7 +26,11 @@ const Navbar = () => {
       {/* Navbar Start */}
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden text-white">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden text-white"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -76,10 +65,62 @@ const Navbar = () => {
       </div>
 
       {/* Navbar End */}
-      <div className="navbar-end">
-        <Link to="/auth/login" className="btn btn-outline  hover:btn-primary">
-          Login
-        </Link>
+      <div className="navbar-end gap-3">
+        {user ? (
+          <div className="dropdown dropdown-end z-50">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-9 border-2 border-gray-300 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  referrerPolicy="no-referrer"
+                  src={
+                    user.photoURL ||
+                    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  }
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu  menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
+            >
+              <div className=" pb-3 border-b border-b-gray-200">
+                <li className="text-sm font-bold">{user.displayName}</li>
+                <li className="text-xs">{user.email}</li>
+              </div>
+              <li className="mt-3">
+                <Link to={"/profile"}>
+                  <FaUser /> Profile
+                </Link>
+              </li>
+
+              <li>
+                <Link to={"/my-movies"}>My Movies</Link>
+              </li>
+
+              <li>
+                <button
+                  onClick={signOutUser}
+                  className="btn btn-outline hover:btn-primary"
+                >
+                  <IoLogOut /> Logout
+                </button>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <Link
+            to={"/auth/login"}
+            className="btn btn-outline hover:btn-primary"
+          >
+            {" "}
+            <IoLogIn /> Login
+          </Link>
+        )}
       </div>
     </div>
   );
