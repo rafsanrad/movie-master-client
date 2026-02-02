614a56377a27a58f1abbae531085339302468927
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-02-02 21:48:09 +0600

    added profile

--- src/pages/MyMovies.jsx
+++ src/pages/MyMovies.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-
+import { use, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { MovieCard } from "../components/MovieCard";
+import LoadingSpinner from "../components/LoadingSpinner";
 const MyMovies = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { user } = use(AuthContext);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/my-movies?email=${user.email}`, {
+      headers: {
+        authorization: `Bearer ${user.accessToken}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data);
+        setLoading(false);
+      });
+  }, [user]);
+
+  if (loading) {
+    return <LoadingSpinner></LoadingSpinner>;
+  }
+
+  return (
+    <div className="w-11/12 mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {movies.map((movie) => (
+          <MovieCard key={movie._id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyMovies;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 
 const Profile = () => {
-    return (
-        <div>
-            
+  const { user } = useContext(AuthContext);
+  // console.log(user);
+  return (
+    <div className=" flex items-center justify-center bg-base-200 mt-10">
+      <div className="card w-96 bg-base-100 shadow-xl text-center">
+        <div className="card-body items-center">
+          {/* Profile Image */}
+          <img
+            src={user?.photoURL || "https://i.ibb.co/ZYW3VTp/brown-brim.png"}
+            alt="User"
+            className="w-24 h-24 rounded-full mb-4"
+          />
+
+          {/* Name */}
+          <h2 className="card-title">{user?.displayName || "No Name"}</h2>
+
+          {/* Email */}
+          <p className="text-gray-500">{user?.email}</p>
+
+          {/* Status */}
+          <span className="badge badge-success mt-3">Logged In</span>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
